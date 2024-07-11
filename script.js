25021d6d1606ed5867b725561fe3d508b9e3a629
Kapsetus <Tushar.Kapse@draeger.com>
2024-07-11 10:58:34 -0400

    updated:script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const locationInput = document.getElementById('locationInput');
 const searchButton = document.getElementById('searchButton');
-const locationElement = document.getElementById('location');
+//const locationElement = document.getElementById('location');
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
 let iconElement = document.getElementById('icon');
-const containerElement = document.getElementById('container');
+//const containerElement = document.getElementById('container');
 
 searchButton.addEventListener('click', () => {
     const location = locationInput.value;
